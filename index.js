78a671116bab0b824b0f710eed693b3d5181d104
Shubhendu <shubhendu.saurabh@hotmail.com>
2014-09-23 19:31:36 +0530

    chatroom working

--- index.js
+++ index.js
@@ -20,16 +20,68 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(methodOverride());
 
 if ('development' == env) {
     app.use(errorhandler());
+    app.locals.pretty = true;
 }
 
-var users = [];
+var usernames = {};
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+    var addedUser = false;
+
+    socket.on('new message', function (data) {
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    socket.on('add user', function (username) {
+        socket.username = username;
+        usernames[username] = username;
+        numUsers += 1;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+
+    socket.on('typing', function () {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+
+    socket.on('stop typing', function () {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+
+    socket.on('disconnect', function () {
+        if (addedUser) {
+            delete usernames[socket.username];
+            numUsers -= 1;
+
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
+});
 
 app.get('/', function(req, res){
     res.render('index', {
@@ -41,20 +93,4 @@ app.get('/', function(req, res){
 
 app.get('/available', function(req, res){
     res.send({available: users.length});
-});
-
-io.sockets.on('connection', function(socket) {
-    socket.on("hello", function(jid) {
-        users.push({socket: socket.id.toString(), jid: jid});
-        io.sockets.emit("queue", users.length);
-    });
-    socket.on("disconnect", function() {
-        var userFilter = users.filter(function (user) {
-            return user.socket == socket.id.toString()
-        });
-        if (userFilter && userFilter.length) {
-            users.splice(users.indexOf(userFilter[0]), 1);
-            io.sockets.emit("queue", users.length);
-        }
-    });
 });
